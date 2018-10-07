8ee535c18c786cd8fd0e10c9b6fb719da3205cb5
Matt Dennis <dennis57@msu.edu>
2018-10-06 23:29:00 -0400

    create the base layout for the game activity

---
+++ app/src/main/java/edu/msu/team15/connect4/ConnectFourView.java
@@ -0,0 +1,37 @@
+package edu.msu.team15.connect4;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * The view we will draw out hatter in
+ */
+public class ConnectFourView extends View {
+    public ConnectFourView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public ConnectFourView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public ConnectFourView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+
+    }
+}
