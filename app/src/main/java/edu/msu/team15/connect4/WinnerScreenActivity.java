b8aa76f17571201533bd45bad81183ebce77c1c2
Matt Dennis <dennis57@msu.edu>
2018-10-11 13:47:52 -0400

    make restart button go to home screen

--- app/src/main/java/edu/msu/team15/connect4/WinnerScreenActivity.java
+++ app/src/main/java/edu/msu/team15/connect4/WinnerScreenActivity.java
@@ -1,7 +1,9 @@
 package edu.msu.team15.connect4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WinnerScreenActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class WinnerScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_winner_screen);
+    }
+
+    public void onRestart(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
