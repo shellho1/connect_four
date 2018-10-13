ea9d263c60b8d43f60530d17abc89a339dc42ff0
Weston Shellhorn <shellho1@cse.msu.edu>
2018-10-12 22:09:46 -0400

    Created instruction dialog box
    Signed-off-by: Weston Shellhorn <shellho1@cse.msu.edu>

--- app/src/main/java/edu/msu/team15/connect4/MainActivity.java
+++ app/src/main/java/edu/msu/team15/connect4/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.msu.team15.connect4;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,5 +20,20 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ConnectFourActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    public void onInstruct(View view){
+        // User has clicked the how to play button
+        // Instantiate a dialog box builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+
+        // Parameterize the builder
+        builder.setTitle(R.string.instruction);
+        builder.setMessage(R.string.instruct_body);
+        builder.setPositiveButton(android.R.string.ok, null);
+
+        // Create the dialog box and show it
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
