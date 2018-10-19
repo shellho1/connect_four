d671bd9aae91ac3c67bec422ba1817cd293f4c25
Weston Shellhorn <shellho1@cse.msu.edu>
2018-10-18 20:48:43 -0400

    Player names update after each turn on gameplay screen
    Signed-off-by: Weston Shellhorn <shellho1@cse.msu.edu>

--- app/src/main/java/edu/msu/team15/connect4/ConnectFourActivity.java
+++ app/src/main/java/edu/msu/team15/connect4/ConnectFourActivity.java
@@ -3,7 +3,9 @@ package edu.msu.team15.connect4;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class ConnectFourActivity extends AppCompatActivity {
 
@@ -11,6 +13,11 @@ public class ConnectFourActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect_four);
+
+        Intent intent = getIntent();
+        String player_one = intent.getStringExtra("p1");
+        TextView initial_turn = findViewById(R.id.playerText);
+        initial_turn.setText(player_one);
     }
 
     public void onSurrender(View view) {
@@ -25,6 +32,19 @@ public class ConnectFourActivity extends AppCompatActivity {
     public void onDone(View view) {
         if (!getConnectFourView().getConnectFour().endTurn()) {
             //TODO: pop up dialog that turn is not ended cause they didnt play
+        }
+        else {
+            Intent intent = getIntent();
+            String player_one = intent.getStringExtra("p1");
+            String player_two = intent.getStringExtra("p2");
+            TextView turn = findViewById(R.id.playerText);
+            if (player_one == turn.getText()){
+                turn.setText(player_two);
+            }
+            else {
+                turn.setText(player_one);
+            }
+            turn.invalidate();
         }
     }
 
