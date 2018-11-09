4f9ccf2cde0f4f78a8be3608be607b9ad246aada
Matt Dennis <dennis57@msu.edu>
2018-11-08 20:38:43 -0500

    created a cloud class and added the magic and URLs for login and create user

---
+++ android/app/src/main/java/edu/msu/team15/connect4/Cloud.java
@@ -0,0 +1,13 @@
+package edu.msu.team15.connect4;
+
+public class Cloud {
+    private static final String MAGIC = "NechAtHa6RuzeR8x";
+
+    // Login and Create User expect user, pw, and magic as get params
+    private static final String LOGIN_URL = "https://webdev.cse.msu.edu/~dennis57/cse476/project2/login-user.php";
+    private static final String CREATE_USER_URL = "https://webdev.cse.msu.edu/~dennis57/cse476/project2/create-user.php";
+    
+    private static final String UTF8 = "UTF-8";
+
+
+}
