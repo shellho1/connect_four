361f1a69dd62e275e777f8df7505c4ec21f110f2
Weston Shellhorn <shellho1@cse.msu.edu>
2018-11-10 00:32:44 -0500

    login and create user functions

--- app/src/main/java/edu/msu/team15/connect4/Cloud.java
+++ app/src/main/java/edu/msu/team15/connect4/Cloud.java
@@ -1,5 +1,11 @@
 package edu.msu.team15.connect4;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Cloud {
     private static final String MAGIC = "NechAtHa6RuzeR8x";
 
@@ -9,5 +15,54 @@ public class Cloud {
     
     private static final String UTF8 = "UTF-8";
 
+    public InputStream loginUser(final String username, final String password) {
+        // Create a get query
+        String query = LOGIN_URL + "?user=" + username + "&magic=" + MAGIC + "&pw=" + password;
+
+        try {
+            URL url = new URL(query);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            int responseCode = conn.getResponseCode();
+            if(responseCode != HttpURLConnection.HTTP_OK) {
+                return null;
+            }
+
+            InputStream stream = conn.getInputStream();
+            return stream;
+
+        } catch (MalformedURLException e) {
+            // Should never happen
+            return null;
+        } catch (IOException ex) {
+            return null;
+        }
+    }
+
+    public InputStream createUser(final String username, final String password) {
+        // Create a get query
+        String query = CREATE_USER_URL + "?user=" + username + "&magic=" + MAGIC + "&pw=" + password;
+
+        try {
+            URL url = new URL(query);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            int responseCode = conn.getResponseCode();
+            if(responseCode != HttpURLConnection.HTTP_OK) {
+                return null;
+            }
+
+            InputStream stream = conn.getInputStream();
+            return stream;
+
+        } catch (MalformedURLException e) {
+            // Should never happen
+            return null;
+        } catch (IOException ex) {
+            return null;
+        }
+    }
+
 
 }
+
