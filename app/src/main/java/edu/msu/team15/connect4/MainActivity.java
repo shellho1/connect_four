5b5fa56706f262bd4f7eceac8e477979f7f066a6
Matt Dennis <dennis57@msu.edu>
2018-11-12 12:15:47 -0500

    create write preferences function for remember

--- app/src/main/java/edu/msu/team15/connect4/MainActivity.java
+++ app/src/main/java/edu/msu/team15/connect4/MainActivity.java
@@ -1,6 +1,8 @@
 package edu.msu.team15.connect4;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
     private static final String PLAYER2_DEFAULT = "player2";
     public static final String PLAYER1_NAME = "p1";
     public static final String PLAYER2_NAME = "p2";
+    private static final String REMEMBER_CHECKED = "checked";
+    private static final String USER_PREF = "userid";
+    private static final String PASS_PREF = "password";
+
+    private SharedPreferences settings = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +31,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void onStart(View view) {
         EditText player1 = findViewById(R.id.userEditText);
-        String p1 = player1.getText().toString().equals("") ? PLAYER1_DEFAULT : player1.getText().toString();
+        String userid = player1.getText().toString();
 
-        EditText player2 = findViewById(R.id.password2EditText);
-        String p2 = player2.getText().toString().equals("") ? PLAYER2_DEFAULT : player2.getText().toString();
+        EditText player2 = findViewById(R.id.passwordEditText);
+        String password = player2.getText().toString();
 
         Intent intent = new Intent(this, ConnectFourActivity.class);
-        intent.putExtra(PLAYER1_NAME, p1);
-        intent.putExtra(PLAYER2_NAME, p2);
+        intent.putExtra(PLAYER1_NAME, userid);
+        intent.putExtra(PLAYER2_NAME, password);
         startActivity(intent);
         finish();
     }
@@ -55,5 +62,15 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, CreateUserActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    private void writePreferences(String userid, String password) {
+        settings = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = settings.edit();
+
+        editor.putString(USER_PREF, userid);
+        editor.putString(PASS_PREF, password);
+
+        editor.apply();
     }
 }
